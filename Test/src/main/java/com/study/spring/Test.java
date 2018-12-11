916f4023622075a81bd0310719426a0a56ce23ab
RXXAND <RXXAND@RD0063252.global.scd.scania.com>
2018-12-11 22:10:53 +0100

    for loacl repository

--- Test/src/main/java/com/study/spring/Test.java
+++ Test/src/main/java/com/study/spring/Test.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	System.out.println("main method added");
 	System.out.println("test");
 	System.out.println("deleted");
+	
+	System.out.println("Chanegs at local repository");
 }
 }
