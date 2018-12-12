83c077169cb9b8116c709d7a1f12efea9bc3a758
RXXAND <RXXAND@RD0063252.global.scd.scania.com>
2018-12-12 20:22:58 +0100

    akshay changes added

--- Test/src/main/java/com/study/spring/Test.java
+++ Test/src/main/java/com/study/spring/Test.java
@@ -10,6 +10,8 @@ public static void main(String[] args) {
 	System.out.println("Chanegs at local repository");
 
 	System.out.println("created new local repository");
+	
+	System.out.println("akshay changes");
 
   
 }
